162a35a3703dd27da5aeb7f3b082e5e39dae12ec
PedroLiLL <106795711+PedroLiLL@users.noreply.github.com>
2023-03-16 18:16:41 -0500

    Add files via upload

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,24 +1,3 @@
-class Vehiculo:
-    
-    def Color(self,c):
-        return c
-    
-    def Ruedas(self,r):
-        return r
-    
-    def Puertas(self,p):
-        return p
-    
-class Coche(Vehiculo):
-    def Velocidad(self,v):
-        return v
-    
-    def Cilindrica(self, cil):
-        return cil
-
-C = Coche()
-print("Color:",C.Color("Azul"))
-print("Numero de ruedas:",C.Ruedas(4))
-print("Numero de puertas:",C.Puertas(4))
-print("Velocidad maxima:",C.Velocidad(350),"km/h")
-print("Cilindrica:",C.Cilindrica(200))
+items = input("Introduce países separados por comas:\n")
+paises = [pais for pais in items.split(",")]
+print(",".join(sorted(list(set(paises)))))
